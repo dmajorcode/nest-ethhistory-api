006ed2a17b637268968e0fa61b6bc5fafdf7582e
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2023-03-11 15:04:23 +0900

    add: infura provider

--- src/blocks/blocks.service.ts
+++ src/blocks/blocks.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { ethers } from 'ethers';
 import { Model } from 'mongoose';
 import { Blocks, BlocksDocument } from 'src/schemas/blocks.schema';
 
 @Injectable()
 export class BlocksService {
+  public static provider: ethers.providers.InfuraProvider;
   constructor(
     @InjectModel('Blocks') private blocksModel: Model<BlocksDocument>, // @InjectModel('Blocks') private blocksModel: Model<BlocksDocument>,
-  ) {}
+  ) {
+    this.getBlock();
+  }
 
   async findOne(): Promise<Blocks> {
     const sampleBlock = this.blocksModel.findOne();
     return sampleBlock;
+  }
+
+  async getBlock() {
+    const provider = new ethers.providers.InfuraProvider(
+      process.env.NETWORK,
+      process.env.INFURA_API_KEY,
+    );
   }
 }
