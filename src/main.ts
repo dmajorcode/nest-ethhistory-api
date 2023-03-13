bcfcd33ec3fbd5dcfc003afef5320ebee9c19b40
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2023-03-13 15:56:06 +0900

    add: sentry

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as Sentry from '@sentry/node';
 import 'dotenv/config';
 import { AppModule } from './app.module';
-
+import { WebhookInterceptor } from './slack-bot/slack.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new WebhookInterceptor());
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [new Sentry.Integrations.Http({ tracing: true })],
+    tracesSampleRate: 1.0,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Block Histroy Bot')
